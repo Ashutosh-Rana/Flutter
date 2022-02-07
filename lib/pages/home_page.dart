b7b7fa2d10670550aa8812000224320e750c1d07
aashishsingh491 <44177460+aashishsingh491@users.noreply.github.com>
2022-02-07 22:34:50 +0530

    day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,10 +2,31 @@ import 'package:first_app/models/catalog.dart';
 import 'package:first_app/widgets/drawer.dart';
 import 'package:first_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
   final String name = "Ashutosh";
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsdata = decodedData["products"];
+    print(productsdata);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(6, (index) => CatalogModel.items[0]);
@@ -32,5 +53,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
